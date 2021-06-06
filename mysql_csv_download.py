ee0697665a3c47d3a26f0a9328494f39bff6eaab
pream010 <61114523+pream010@users.noreply.github.com>
2021-06-06 13:32:43 +0530

    Update mysql_csv_download.py

--- mysql_csv_download.py
+++ mysql_csv_download.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-# importing module 
+# importing module
 import mysql.connector as mysql
 import csv
 import pandas as pd
@@ -8,23 +8,25 @@ import pandas as pd
 ##########START_OF_MySQL Block##########
 #connection estrablish from mysql database
 try:
-    mysqldb = mysql.connect(host='<ip_address>', database='<db_name>', user='<user>', password='<pwd>')
+    mysqldb = mysql.connect(host='<ip>', database='<DB_NAME>', user='<USER_NAME>', password='<PWD>')
     try:
         # MySQL select query
-        mysqlquery = """<SQL Code>"""
-        
+        mysqlquery = """<SQL_QUERY>"""
+
         # fetching all records
         records = pd.read_sql_query(mysqlquery, mysqldb)
 
-        #Save it as csv file in folder:    
-        records.to_csv('<file_location>', index=False)
-        
+        #Save it as csv file in folder:
+        records.to_csv('<PATH>', index=False)
+
     except Exception as mysqlqueryerr:
         print("There is issue with : ", mysqlqueryerr)
-    
-except mysql.connector.Error as mysqlerr: 
+
+except mysql.connector.Error as mysqlerr:
     print("There is a problem with MySQL", mysqlerr)
 
 finally:
     if mysqldb:
         mysqldb.close()
+
+##########END_OF_MySQL Block##########
